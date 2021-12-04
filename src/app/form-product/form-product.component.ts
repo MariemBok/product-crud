4ea6046fe6fb4b182d14fc47e90854005601f2a1
Mariem Bokri <mariem.bokri@esprit.tn>
2021-12-04 19:52:14 +0100

    form product

--- src/app/form-product/form-product.component.ts
+++ src/app/form-product/form-product.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormGroup, Form } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
-  styleUrls: ['./form-product.component.css']
+  styleUrls: ['./form-product.component.css'],
 })
 export class FormProductComponent implements OnInit {
-
-  constructor() { }
+  productForm!: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.productForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      price: new FormControl('', [Validators.required]),
+      category: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      photo: new FormControl('dryfruit.jpg', [Validators.required]),
+    });
   }
-
+  addProduct(productForm: FormGroup) {}
 }
--- src/app/main-product/main-product.component.ts
+++ src/app/main-product/main-product.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { FormGroup, Form } from '@angular/forms';
-import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main-product',
@@ -11,23 +8,10 @@ import { Validators } from '@angular/forms';
 export class MainProductComponent implements OnInit {
   isForm: Boolean = false;
   buttonString: String = 'Add New Product';
-  productForm!: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {
-    this.productForm = new FormGroup({
-      title: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-      ]),
-      price: new FormControl('', [Validators.required]),
-      category: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-      ]),
-      photo: new FormControl('dryfruit.jpg', [Validators.required]),
-    });
-  }
+  ngOnInit(): void {}
   changePage() {
     this.isForm = !this.isForm;
     if (this.isForm) {
